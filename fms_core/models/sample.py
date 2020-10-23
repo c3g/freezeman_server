934df31b3391cf012e714e34e283b4e2b6d553ed
zxenia <ksenia1zaytseva@gmail.com>
2020-10-23 18:05:57 -0400

    remove _old fields uncomment fields in admin and viewsets

--- fms_core/models/sample.py
+++ fms_core/models/sample.py
@@ -91,8 +91,6 @@ class Sample(models.Model):
                             help_text="Sample name.")
     alias = models.CharField(max_length=200, blank=True, help_text="Alternative sample name given by the "
                                                                    "collaborator or customer.")
-
-    individual_old = models.CharField(blank=True, null=True, max_length=200)
     individual = models.ForeignKey("Individual", on_delete=models.PROTECT, null=True,
                                    help_text="Individual associated with the sample.")
     volume_history = models.JSONField("volume history in µL", validators=[VOLUME_VALIDATOR],
